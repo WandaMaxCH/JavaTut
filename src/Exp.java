a2a2975ed782133b00f8092f0e4db04d0872b8ff
elena <elena@fedora>
2022-10-02 20:28:45 +0200

    Exp class

---
+++ src/Exp.java
@@ -0,0 +1,33 @@
+
+public class Exp {
+
+	public static void main(String[] args) {
+		// type variableName = value;
+
+		String name = "Elena";
+		System.out.println(name);
+		name = "IvU";
+		System.out.println(name);
+		int myNum;
+		myNum = 15;
+		System.out.println(myNum);
+		myNum = 20;
+		System.out.println(myNum);
+		final int Number = 18;
+		// Number = 32;
+		System.out.println("Gruzi" + name);
+		String lastname = "andra";
+		String fullname = name + lastname;
+		System.out.println(fullname);
+		int x = 34;
+		int y = 35;
+		System.out.println(x + y);
+		int c = 2, v = 3, b = 4;
+		System.out.println(c + v + b);
+		int j, k, l;
+		j = k = l = 45;
+		System.out.println(j + k + l);
+
+	}
+
+}
